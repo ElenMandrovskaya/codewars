fcf684312e67f9d3e06a1f0333a16da14dc2e519
Elena <e.mandrovska@gmail.com>
2021-04-17 17:24:41 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -44,8 +44,12 @@
 // }
 // console.log(disemvowel("This website is for losers LOL!"))
 
-function findOutlier(integers){
 
+
+function findOutlier(integers){
+    const even = integers.filter(el => el % 2 === 0);
+    const odd = integers.filter(el => el % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0]
 }
-// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
 console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
