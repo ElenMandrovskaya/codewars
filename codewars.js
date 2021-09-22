32d2dc2d853033151f871092a80dfe7efc492f98
Elena <e.mandrovska@gmail.com>
2021-09-22 17:18:21 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -46,10 +46,10 @@
 
 
 
-function findOutlier(integers){
-    const even = integers.filter(el => el % 2 === 0);
-    const odd = integers.filter(el => el % 2 !== 0);
-    return even.length === 1 ? even[0] : odd[0]
-}
-console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
-console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
+// function findOutlier(integers){
+//     const even = integers.filter(el => el % 2 === 0);
+//     const odd = integers.filter(el => el % 2 !== 0);
+//     return even.length === 1 ? even[0] : odd[0]
+// }
+// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
+// console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
