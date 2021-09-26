bc39e734295ac7ab002c795b35bca1579f9220ea
Elena <e.mandrovska@gmail.com>
2021-09-26 23:37:55 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -148,3 +148,31 @@
 // console.log(pigIt('Pig latin is cool')) //igPay atinlay siay oolcay
 // console.log(pigIt('This is my string !')) //hisTay siay ymay tringsay
 
+
+// Write a function named first_non_repeating_letter that takes a string input, 
+// and returns the first character that is not repeated anywhere in the string.
+// For example, if given the input 'stress', the function should return 't',
+// since the letter t only occurs once in the string, and occurs first in the string.
+// As an added challenge, upper- and lowercase letters are considered the same character, 
+// but the function should return the correct case for the initial letter. For example, 
+// the input 'sTreSS' should return 'T'.
+// If a string contains all repeating characters, it should return an empty string ("") 
+// or None -- see sample tests.
+
+function firstNonRepeatingLetter(s) {
+    // if (!s) return ''
+    const idx = s
+    .toLowerCase()
+    .split('')
+    .findIndex((el, ind, s) => {
+         return s.indexOf(el) === s.lastIndexOf(el)
+    })
+    return idx === -1 ? '' : s[idx]
+  }
+
+  console.log(firstNonRepeatingLetter('a'));
+  console.log(firstNonRepeatingLetter('sTress'));
+  console.log(firstNonRepeatingLetter('moonmen'));
+  console.log(firstNonRepeatingLetter('nonononono'));
+  console.log(firstNonRepeatingLetter(''));
+    
