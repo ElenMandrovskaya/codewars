2a9de56f5b2654f58a453be3e0ba2411e9ebaee4
Elena <e.mandrovska@gmail.com>
2021-09-29 18:07:50 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -243,3 +243,62 @@
 //      [9, 0, 1, 5, 3, 7, 2, 1, 4],
 //      [2, 8, 7, 4, 1, 9, 6, 3, 5],
 //      [3, 0, 0, 4, 8, 1, 1, 7, 9]])); //"Try again!")
+
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+
+// function distinct(a) {
+//      return Array.from(new Set(a))
+// }
+
+// console.log(distinct([1]));
+// console.log(distinct([1,2]));
+// console.log(distinct([1,1,2]));
+
+
+
+// ⚠️ The world is in quarantine! There is a new pandemia that struggles mankind. 
+// Each continent is isolated from each other but infected people have spread before the warning. ⚠️
+// 🗺️ You would be given a map of the world in a type of string:
+// string s = "01000000X000X011X0X"
+// '0' : uninfected
+// '1' : infected
+// 'X' : ocean
+// ⚫ The virus can't spread in the other side of the ocean.
+// ⚫ If one person is infected every person in this continent gets infected too.
+// ⚫ Your task is to find the percentage of human population that got infected in the end.
+// ☑️ Return the percentage % of the total population that got infected.
+// ❗❗ The first and the last continent are not connected!
+// 💡 Example:
+//  start: map1 = "01000000X000X011X0X"
+//  end:   map1 = "11111111X000X111X0X"
+//  total = 15
+//  infected = 11
+//  percentage = 100*11/15 = 73.33333333333333
+// ➕ For maps without oceans "X" the whole world is connected.
+// ➕ For maps without "0" and "1" return 0 as there is no population.
+
+// function infected(s) {
+//    const splitMap = s.split('X');
+//    const total = splitMap.join('').length;
+//    let infected = 0;
+
+//      splitMap.map(el => {el.split('')
+//      if (el.includes('1')) infected += el.length
+//      });
+// if (infected === 0) return 0
+//      return 100*infected/total
+
+// //    console.log(total)
+// //    console.log(infected)
+// //    console.log(percentage)
+//    }
+
+
+// console.log( infected("01000000X000X011X0X")) //, 73.33333333333333, EPSILON );
+// console.log( infected("01X000X010X011XX")) //, 72.72727272727273, EPSILON );
+// console.log( infected("XXXXX")) //, 0, EPSILON );
+// console.log( infected("0000000010")) //, 100, EPSILON );
+// console.log( infected("X00X000000X10X0100")) //, 42.857142857142854, EPSILON );
+// console.log( infected("000000000")) //, 42.857142857142854, EPSILON );
